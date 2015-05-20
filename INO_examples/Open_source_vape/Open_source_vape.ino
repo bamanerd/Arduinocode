ca4163a935267d870cfc7e1900f565c651c22342
Pops <poppavic60@gmail.com>
2015-05-20 14:06:27 -0700

    Rearranged subdir

---
+++ INO_examples/Open_source_vape/Open_source_vape.ino
@@ -0,0 +1,50 @@
+/*
+GNU GPLv3 License
+
+Contributor list:
+Zarboz
+*/
+//list libraries needed here
+#include <avr.h>
+#include <avr/sleep.here>
+#include <Adafruit_GFX.h> 
+#include <Adafruit_SSD1306.h>
+#include <SPI.h>
+
+//declare global vars here
+const int voltPin = 0;
+int voltres1 = 10000;
+int voltres2 = 2500;
+
+#define OLED_MOSI   9
+#define OLED_CLK   10
+#define OLED_DC    14
+#define OLED_CS    15
+#define OLED_RESET 16
+Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+void setup () { 
+  display.begin(SSD1306_SWITCHCAPVCC);
+  display.display(); // show splashscreen
+  delay(500);
+  display.clearDisplay();
+  denominator = (float)voltres2 / (voltres1 + voltres2);
+}
+
+
+void loop(){
+display.setTextColor(WHITE);
+display.setTextSize(4);
+display.print("V:");
+checkvolt;
+display.print(voltage);
+}
+
+static void checkvolt()
+{
+	float voltage;
+	voltage = analogRead(voltPin);
+	voltage = (voltage / 1024) * 5.0;
+	voltage = voltage / denominator;
+	delay (500);
+	}
---
+++ README.txt
@@ -0,0 +1,5 @@
+Insiduous 20150520.1216:
+  Re-arranged the directory. Initial INO didn't work, and I decided that
+  subdirs for non-catastrophic tests was apropos. Things to test and CAN
+  fail can belong therein.
+
